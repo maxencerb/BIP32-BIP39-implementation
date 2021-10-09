3f0ff72f723852ccf4663f5277a89e7cd1e135f9
maxencerb <maxenceraballand00@gmail.com>
2021-10-09 11:30:36 +0200

    merge conflict

--- generate_wallet.py
+++ generate_wallet.py
@@ -3,18 +3,8 @@ from word_list import word_list
 import hashlib
 import sys
 from utils import padd_binary
-<<<<<<< HEAD
 from ecdsa import ECDH, SECP256k1, SigningKey, VerifyingKey
 import binascii
-=======
-from ecdsa.util import PRNG
-from ecdsa import SigningKey, NIST256p
-G = (
-    0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798,
-    0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8,
-    0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141
-)
->>>>>>> 9e6ae83bc3d209037d383d3b6eb69a991fe6e020
 
 def import_mnemonic(mnemonic):
     mnemonic_list = mnemonic.split(" ")
