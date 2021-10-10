7fc502616ed5495af052cff6b81f85f74b38eae4
maxencerb <maxenceraballand00@gmail.com>
2021-10-10 22:51:47 +0200

    finnished

--- generate_wallet.py
+++ generate_wallet.py
@@ -3,6 +3,7 @@ from word_list import word_list
 import hashlib
 from utils import padd_binary, byte_to_binary
 from ecdsa import ECDH, SECP256k1, SigningKey, VerifyingKey
+import hmac
 
 def import_mnemonic(mnemonic: str):
     mnemonic_list = mnemonic.split(" ")
@@ -51,7 +52,6 @@ def get_public_key(private_key: bytes):
     signing_key = SigningKey.from_string(private_key, curve=SECP256k1, hashfunc=hashlib.sha256)
     ecdh = ECDH(curve = SECP256k1, private_key=signing_key)
     public_key: VerifyingKey = ecdh.get_public_key()
-    print(len(public_key.to_string()))
     return public_key.to_string()
 
 def child_key(private_key: bytes, public_key: bytes, chain_code: bytes, derivation_path: List[str]):
@@ -61,8 +61,12 @@ def child_key(private_key: bytes, public_key: bytes, chain_code: bytes, derivati
         return private_key, public_key, chain_code
     index = int(derivation_path[1])
     n = 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141
-    hash = hashlib.pbkdf2_hmac('sha512', public_key, chain_code,index)
-    chain_code_child = hash[:32] + bytes(index)
-    child_private_key = (private_key + hash[32:])
+    # hash = hashlib.pbkdf2_hmac('sha512', public_key, chain_code, index)
+    hash = hmac.new(
+        chain_code, b"\x00" + private_key + index.to_bytes(4, "big"), hashlib.sha512
+    ).digest()
+    chain_code_child = hash[32:]
+    # sum the private key and the first 32 bytes of the hash into a new 32 bytes private key
+    child_private_key = ((int.from_bytes(hash[:32], "big") + int.from_bytes(private_key, "big")) % n).to_bytes(32, "big")
     child_public_key = get_public_key(child_private_key)
     return child_key(child_private_key, child_public_key, chain_code_child, derivation_path[1:])
